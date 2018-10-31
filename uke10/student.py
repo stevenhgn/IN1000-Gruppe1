6281af6447d955d858181a72d006bca1b95a31a3
Steven Hoang Giang Nguyen <shnguyen@delamere.ifi.uio.no>
2018-10-31 13:43:53 +0100

    uke10

---
+++ uke10/student.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self, navn):
+        self._navn = navn
+        self._fagListe = []
+
+    def leggTilFag(self, fag):
+        self._fagListe.append(fag)
+
+    def hentAntallFag(self):
+        return len(self._fagListe)
+
+    def hentStudentNavn(self):
+        return self._navn
+
+    def skrivFagPaaStudent(self):
+        print("Navn:", self._navn)
+
+        for fag in self._fagListe:
+            print(fag.hentFagNavn())
---
+++ uke10/testStudentSystem.py
@@ -0,0 +1,9 @@
+from fag import Fag
+from student import Student
+
+class main:
+
+    in1000 = Fag("IN1000")
+    nora = Student("Nora")
+    nora.leggTilFag(in1000)
+    nora.skrivFagPaaStudent()
